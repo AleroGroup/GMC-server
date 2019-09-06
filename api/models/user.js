904f82694533a5a2ac7bab4921d71d9ebdab3f28
Stephanie Kirathe <stephaniekirathe@gmail.com>
2019-09-06 04:41:20 +0300

    removed bycrpt

--- api/models/user.js
+++ api/models/user.js
@@ -22,17 +22,5 @@ const UserSchema = new mongoose.Schema({
         required: true,
     }
 });
-
-UserSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-        if (err) {
-            return next(err);
-        }
-        user.password = hash;
-        next();
-    })
-});
-
 const user = mongoose.model('user', UserSchema);
 module.exports = user;
